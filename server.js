c2d524563bd7a583ab7b440140c0b04bae6c314e
RishikShr <48129871+RishikShr@users.noreply.github.com>
2020-07-08 01:57:48 +0530

    content-type

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const hostname ='127.0.0.1';
 
 const server = http.createServer((req,res)=>{
     res.statusCode=200;
-    // res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain');
     res.end("hello world");
 })
  server.listen(port,hostname, ()=> console.log(`Server	running	at	http://${hostname}:${port}/`));
